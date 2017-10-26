d9484350ef5955da782975dd0fda00f203d1066e
ZHT <1296704020@qq.com>
2017-10-26 19:18:55 +0800

    Git测试

---
+++ src/com/opensource/test.java
@@ -0,0 +1,10 @@
+package com.opensource;
+
+public class test {
+	public static void main(String[] args) {
+		int a = 0;
+		String w="qwdcscsz";
+		a = w.length();
+		System.out.println("字符串w的长度为"+ a);
+	}
+}
